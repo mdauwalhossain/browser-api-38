633e797b4b7b4f5e62e91bd562ef789a801fa467
mdauwalhossain <mdauwalhossain2020@gmail.com>
2021-09-04 23:47:00 +0600

    test

--- js/alert.js
+++ js/alert.js
@@ -1,2 +1,15 @@
-console.log('Ma is coming')
-alert('AMMA')
+// console.log('Ma is coming')
+// alert('AMMA')
+const maComing = () => {
+    alert('Amma is coming!!! Hurry up.')
+}
+const askPicnic = () => {
+    const question = confirm('Wanna go with us???');
+    console.log(question);
+    if (question === true) {
+        alert('fee ta bKash kor');
+    }
+    else {
+        console.log('Hala, dure giye mor')
+    }
+}
